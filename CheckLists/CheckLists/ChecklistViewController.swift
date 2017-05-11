56241485718598006c943a7ad34433f4d6a5dc57
wen <wenzhang@wendeMBP.lan>
2017-05-11 18:56:58 +0800

    checkList

---
+++ CheckLists/CheckLists/ChecklistViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  CheckLists
+//
+//  Created by wen zhang on 2017/5/11.
+//  Copyright © 2017年 Gwen. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath)
+        return cell
+    }
+
+}
+
